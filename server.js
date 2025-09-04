3f6cd36abc231d09604450b4be30d4ea35105c40
Mohd Saif Khan <mkhan2@tsp.tech>
2025-09-04 10:27:45 +0000

    Initial commit of all files

--- server.js
+++ server.js
@@ -1,34 +1,9 @@
-// const cds = require('@sap/cds');
+const cds = require('@sap/cds')
 
-// cds.on('bootstrap', app => {
-//   const cors = require('cors');
-//   app.use(cors({
-//     // origin: 'http://localhost:8080',
-//     origin: [
-//       'https://port8080-workspaces-ws-qfx8w.eu10.applicationstudio.cloud.sap',
-//       'http://localhost:8080' // Include all required origins
-//   ],
-//     credentials: true,
-//     allowedHeaders: ['x-csrf-token', 'content-type', 'authorization'],
-//     exposedHeaders: ['x-csrf-token']
-//   }));
-// });
+cds.on('bootstrap', (app) => {
+  app.get('/health', (req, res) => {
+    res.status(200).send('OK')
+  })
+})
 
-// module.exports = cds.server;
-
-
-
-
-const cds = require('@sap/cds');
-
-cds.on('bootstrap', app => {
-  app.get('/', (req, res, next) => {
-    const accept = req.headers['accept'] || '';
-    if (accept.includes('text/html')) {
-      return res.redirect('/supplier-invoice-ui/webapp');
-    }
-    next();
-  });
-});
-
-module.exports = cds.server;
+module.exports = cds.server
